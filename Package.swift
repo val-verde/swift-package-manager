8781803b23e7343c48d3faf4db31587934b29ced
Aaditya Chandrasekhar <aadityac@gmail.com>
2020-11-13 17:49:34 -0800

    Use val-verde for swift-argument-parser, swift-driver, swift-llbuild and swift-tools-support-core

--- Package.swift
+++ Package.swift
@@ -346,7 +346,7 @@ let relatedDependenciesBranch = "main"
 if ProcessInfo.processInfo.environment["SWIFTPM_LLBUILD_FWK"] == nil {
     if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
         package.dependencies += [
-            .package(url: "https://github.com/apple/swift-llbuild.git", .branch(relatedDependenciesBranch)),
+            .package(url: "https://github.com/val-verde/swift-llbuild.git", .branch("val-verde-mainline")),
         ]
     } else {
         // In Swift CI, use a local path to llbuild to interoperate with tools
@@ -360,13 +360,13 @@ if ProcessInfo.processInfo.environment["SWIFTPM_LLBUILD_FWK"] == nil {
 
 if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     package.dependencies += [
-        .package(url: "https://github.com/apple/swift-tools-support-core.git", .branch(relatedDependenciesBranch)),
+        .package(url: "https://github.com/val-verde/swift-tools-support-core.git", .branch("val-verde-mainline")),
         // The 'swift-argument-parser' version declared here must match that
         // used by 'swift-driver' and 'sourcekit-lsp'. Please coordinate
         // dependency version changes here with those projects.
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.4.3")),
-        .package(url: "https://github.com/apple/swift-driver.git", .branch(relatedDependenciesBranch)),
-        .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMinor(from: "1.1.4")),
+        .package(url: "https://github.com/val-verde/swift-argument-parser.git", .branch("val-verde-mainline")),
+        .package(url: "https://github.com/val-verde/swift-driver.git", .branch("val-verde-mainline")),
+        .package(url: "https://github.com/val-verde/swift-crypto.git", .branch("val-verde-mainline")),
     ]
 } else {
     package.dependencies += [
